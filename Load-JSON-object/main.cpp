4d45323f0cb892be5f97a136f26d76a2802038c8
Erick Veil <eveil@comtechcom.net>
2016-04-04 14:40:16 -0700

    Loading file to string.

--- Load-JSON-object/main.cpp
+++ Load-JSON-object/main.cpp
@@ -1,8 +1,26 @@
 #include <QCoreApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    // load document from file to string
+    QString file_path="/home/eveil/src/dev/qt-json/Load-JSON-object/test.json";
+    QFile file_obj(file_path);
+    if(!file_obj.open(QIODevice::ReadOnly)){
+        qDebug()<<"Cannot open file: "<< file_path;
+    }
+    QTextStream file_text(&file_obj);
+    qDebug()<<"File contents:"<<endl<<file_text.readAll()<<endl;
+
+    // convert string to json object
+
+    // get expected members of the object
+
+    // print the vlaues
 
     return a.exec();
 }
