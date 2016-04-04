f8b3ac294773f354fbe8dbcbf6ab18b782246ac1
Erick Veil <eveil@comtechcom.net>
2016-04-04 15:19:17 -0700

    Loading json text into a json doc
    it's failing

--- Load-JSON-object/Load-JSON-object.pro
+++ Load-JSON-object/Load-JSON-object.pro
@@ -11,6 +11,7 @@ QT       -= gui
 TARGET = Load-JSON-object
 CONFIG   += console
 CONFIG   -= app_bundle
+CONFIG += c++11
 
 TEMPLATE = app
 
--- Load-JSON-object/main.cpp
+++ Load-JSON-object/main.cpp
@@ -2,6 +2,8 @@
 #include <QFile>
 #include <QTextStream>
 #include <QDebug>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 int main(int argc, char *argv[])
 {
@@ -12,11 +14,37 @@ int main(int argc, char *argv[])
     QFile file_obj(file_path);
     if(!file_obj.open(QIODevice::ReadOnly)){
         qDebug()<<"Cannot open file: "<< file_path;
+        exit(1);
     }
     QTextStream file_text(&file_obj);
     qDebug()<<"File contents:"<<endl<<file_text.readAll()<<endl;
+    QString json_string;
+    json_string=file_text.readAll();
+    file_obj.close();
 
     // convert string to json object
+    auto json_doc =
+            QJsonDocument::fromJson(json_string.toLocal8Bit());
+    if(json_doc.isNull()){
+        qDebug()<<"Failed to convert to json doc";
+        exit(2);
+    }
+
+    if(!json_doc.isObject()){
+        qDebug()<<"JSON is not an object.";
+        exit(3);
+    }
+
+    QJsonObject json_obj=json_doc.object();
+
+    if(json_obj.isEmpty()){
+        qDebug()<<"JSON object is empty.";
+        exit(4);
+    }
+
+    qDebug()<<"JSON object: "<<endl<<json_obj;
+
+
 
     // get expected members of the object
 
