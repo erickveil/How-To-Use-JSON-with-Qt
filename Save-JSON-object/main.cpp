2fb34f6f1349efce0c64ab8d3f057fc377e68890
Erick Veil <eveil@comtechcom.net>
2016-04-06 08:55:05 -0700

    Finished working save simple object

--- Save-JSON-object/main.cpp
+++ Save-JSON-object/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     QString character_name="Thor";
-    int strength_stat=34;
+    int strength_stat=35;
     QString enemy_name="Loki";
 
     QJsonObject json_obj;
@@ -17,6 +17,8 @@ int main(int argc, char *argv[])
     json_obj["name"]=character_name;
     json_obj["str"]=strength_stat;
     json_obj["enemy"]=enemy_name;
+
+    qDebug() << json_obj;
 
     QString file_path = "/home/eveil/test_obj.json";
     QFile save_file(file_path);
@@ -26,7 +28,16 @@ int main(int argc, char *argv[])
     }
 
     QJsonDocument json_doc(json_obj);
-    save_file.write(json_doc.toJson());
+
+    qDebug() << json_doc;
+
+    QString json_string = json_doc.toJson();
+
+    qDebug() << json_string;
+
+    save_file.write(json_string.toLocal8Bit());
+    //save_file.write("Goodbye world");
+    save_file.close();
 
     return a.exec();
 }
